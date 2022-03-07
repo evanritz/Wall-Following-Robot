3047778375c6d46aed60debce291977b7772813f
Zarkos99 <91138912+Zarkos99@users.noreply.github.com>
2022-03-07 17:24:56 -0500

    Add files via upload

---
+++ workin_v4/build/default/debug/util.o.d
@@ -0,0 +1,5 @@
+build/default/debug/util.o: util.c util.h stdtypes.h
+
+util.h:
+
+stdtypes.h:
---
+++ workin_v4/build/default/production/MtrCtrl.o.d
@@ -0,0 +1,8 @@
+build/default/production/MtrCtrl.o: MtrCtrl.c config.h stdtypes.h \
+ MtrCtrl.h
+
+config.h:
+
+stdtypes.h:
+
+MtrCtrl.h:
