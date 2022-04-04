df28824bb5d2cee7c6fec6e2fd0ed27bb636d50e
Zarkos99 <91138912+Zarkos99@users.noreply.github.com>
2022-04-04 18:39:44 -0400

    Add files via upload

---
+++ Puppy_Dog_v1/LEDS.c
@@ -0,0 +1,45 @@
+/* 
+ * File:   LEDS.c
+ * Author: Evan
+ *
+ * Ease of use functions for LEDS
+ * 
+ * Created on March 22, 2022, 6:30 PM
+ */
+
+#include "LEDS.h"
+#include "config.h"
+#include <plib.h>
+
+// LED1 //////////////////////////////////
+void LED1_on(void) {
+    prtLed1Set = (1 << bnLed1);
+}
+void LED1_off(void) {
+    prtLed1Clr	= (1 << bnLed1);
+}
+
+// LED2 //////////////////////////////////
+void LED2_on(void) {
+    prtLed2Set = (1 << bnLed2);
+}
+void LED2_off(void) {
+    prtLed2Clr	= (1 << bnLed2);
+}
+
+
+// LED3 //////////////////////////////////
+void LED3_on(void) {
+    prtLed3Set = (1 << bnLed3);
+}
+void LED3_off(void) {
+    prtLed3Clr	= (1 << bnLed3);
+}
+
+// LED4 //////////////////////////////////
+void LED4_on(void) {
+    prtLed4Set = (1 << bnLed4);
+}
+void LED4_off(void) {
+    prtLed4Clr	= (1 << bnLed4);
+}
---
+++ Puppy_Dog_v1/nbproject/Makefile-default.mk
@@ -0,0 +1,260 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=
+else
+COMPARISON_BUILD=
+endif
+
+ifdef SUB_IMAGE_ADDRESS
+
+else
+SUB_IMAGE_ADDRESS_COMMAND=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=main.c MtrCtrl.c spi.c util.c DISPLAY.c LEDS.c OPTICAL_SENSOR.c PID_CTRL.c MTR_CTRL.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/MtrCtrl.o ${OBJECTDIR}/spi.o ${OBJECTDIR}/util.o ${OBJECTDIR}/DISPLAY.o ${OBJECTDIR}/LEDS.o ${OBJECTDIR}/OPTICAL_SENSOR.o ${OBJECTDIR}/PID_CTRL.o ${OBJECTDIR}/MTR_CTRL.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/MtrCtrl.o.d ${OBJECTDIR}/spi.o.d ${OBJECTDIR}/util.o.d ${OBJECTDIR}/DISPLAY.o.d ${OBJECTDIR}/LEDS.o.d ${OBJECTDIR}/OPTICAL_SENSOR.o.d ${OBJECTDIR}/PID_CTRL.o.d ${OBJECTDIR}/MTR_CTRL.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/MtrCtrl.o ${OBJECTDIR}/spi.o ${OBJECTDIR}/util.o ${OBJECTDIR}/DISPLAY.o ${OBJECTDIR}/LEDS.o ${OBJECTDIR}/OPTICAL_SENSOR.o ${OBJECTDIR}/PID_CTRL.o ${OBJECTDIR}/MTR_CTRL.o
+
+# Source Files
+SOURCEFILES=main.c MtrCtrl.c spi.c util.c DISPLAY.c LEDS.c OPTICAL_SENSOR.c PID_CTRL.c MTR_CTRL.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=32MX460F512L
+MP_LINKER_FILE_OPTION=
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assembleWithPreprocess
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/7b3f4c0d09e6df3330b3031959b278a9e88e88e .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/MtrCtrl.o: MtrCtrl.c  .generated_files/flags/default/ff4ae9985630612046cecaecd8d933b2213f0ad0 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/MtrCtrl.o.d 
+	@${RM} ${OBJECTDIR}/MtrCtrl.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/MtrCtrl.o.d" -o ${OBJECTDIR}/MtrCtrl.o MtrCtrl.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/spi.o: spi.c  .generated_files/flags/default/ba8a6069299dbb9702b27ceb3f61dfdfab86843d .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/spi.o.d 
+	@${RM} ${OBJECTDIR}/spi.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/spi.o.d" -o ${OBJECTDIR}/spi.o spi.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/util.o: util.c  .generated_files/flags/default/d3c397ac97926388d4aefcdf666bc719f269aab8 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/util.o.d 
+	@${RM} ${OBJECTDIR}/util.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/util.o.d" -o ${OBJECTDIR}/util.o util.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/DISPLAY.o: DISPLAY.c  .generated_files/flags/default/f3365255d6dbdb78b8524e733f985a32d8c3aab3 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/DISPLAY.o.d 
+	@${RM} ${OBJECTDIR}/DISPLAY.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/DISPLAY.o.d" -o ${OBJECTDIR}/DISPLAY.o DISPLAY.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/LEDS.o: LEDS.c  .generated_files/flags/default/5091ce02b7500fdcbc25290b760c21ee8d84a310 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/LEDS.o.d 
+	@${RM} ${OBJECTDIR}/LEDS.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/LEDS.o.d" -o ${OBJECTDIR}/LEDS.o LEDS.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/OPTICAL_SENSOR.o: OPTICAL_SENSOR.c  .generated_files/flags/default/3af1fedc21029b7bebc136cb4206b10e5ee72c74 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/OPTICAL_SENSOR.o.d 
+	@${RM} ${OBJECTDIR}/OPTICAL_SENSOR.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/OPTICAL_SENSOR.o.d" -o ${OBJECTDIR}/OPTICAL_SENSOR.o OPTICAL_SENSOR.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/PID_CTRL.o: PID_CTRL.c  .generated_files/flags/default/6d8d8c83ad47193206246462bb748281ec8441f1 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PID_CTRL.o.d 
+	@${RM} ${OBJECTDIR}/PID_CTRL.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/PID_CTRL.o.d" -o ${OBJECTDIR}/PID_CTRL.o PID_CTRL.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/MTR_CTRL.o: MTR_CTRL.c  .generated_files/flags/default/63cb157edb86933edf30dfb95e2c205b57552cf5 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/MTR_CTRL.o.d 
+	@${RM} ${OBJECTDIR}/MTR_CTRL.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/MTR_CTRL.o.d" -o ${OBJECTDIR}/MTR_CTRL.o MTR_CTRL.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+else
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/59fc621fa4631e70d7d70c6297fcc08248b35085 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/MtrCtrl.o: MtrCtrl.c  .generated_files/flags/default/6d67cd648690d52cd4cd48e134c87cf4adb7639f .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/MtrCtrl.o.d 
+	@${RM} ${OBJECTDIR}/MtrCtrl.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/MtrCtrl.o.d" -o ${OBJECTDIR}/MtrCtrl.o MtrCtrl.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/spi.o: spi.c  .generated_files/flags/default/87483aeb25cf229761149db9152bffe521951bea .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/spi.o.d 
+	@${RM} ${OBJECTDIR}/spi.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/spi.o.d" -o ${OBJECTDIR}/spi.o spi.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/util.o: util.c  .generated_files/flags/default/38e98865d45bad0d97362179481d3e71c433b4f3 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/util.o.d 
+	@${RM} ${OBJECTDIR}/util.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/util.o.d" -o ${OBJECTDIR}/util.o util.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/DISPLAY.o: DISPLAY.c  .generated_files/flags/default/f730d80393ccd8818db17b987d3cdf0d3250e4b9 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/DISPLAY.o.d 
+	@${RM} ${OBJECTDIR}/DISPLAY.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/DISPLAY.o.d" -o ${OBJECTDIR}/DISPLAY.o DISPLAY.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/LEDS.o: LEDS.c  .generated_files/flags/default/b59712c790ecd614a02f7b29d9efd5ff2751385 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/LEDS.o.d 
+	@${RM} ${OBJECTDIR}/LEDS.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/LEDS.o.d" -o ${OBJECTDIR}/LEDS.o LEDS.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/OPTICAL_SENSOR.o: OPTICAL_SENSOR.c  .generated_files/flags/default/c452fb6fdd6ab54253dbe44fd5e507ae6889cf2 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/OPTICAL_SENSOR.o.d 
+	@${RM} ${OBJECTDIR}/OPTICAL_SENSOR.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/OPTICAL_SENSOR.o.d" -o ${OBJECTDIR}/OPTICAL_SENSOR.o OPTICAL_SENSOR.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/PID_CTRL.o: PID_CTRL.c  .generated_files/flags/default/47354fbf5806677032cdc07a938ef8a16250d5f8 .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PID_CTRL.o.d 
+	@${RM} ${OBJECTDIR}/PID_CTRL.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/PID_CTRL.o.d" -o ${OBJECTDIR}/PID_CTRL.o PID_CTRL.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+${OBJECTDIR}/MTR_CTRL.o: MTR_CTRL.c  .generated_files/flags/default/d668578ccfa1fdb39069396e44bf3b3b56f7bf9e .generated_files/flags/default/f2d2849a63ebd9d9bb029b271492fb5470a4c3d1
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/MTR_CTRL.o.d 
+	@${RM} ${OBJECTDIR}/MTR_CTRL.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -I"." -MP -MMD -MF "${OBJECTDIR}/MTR_CTRL.o.d" -o ${OBJECTDIR}/MTR_CTRL.o MTR_CTRL.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compileCPP
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x0:0x1FC -mreserve=boot@0x1FC02000:0x1FC02FEF -mreserve=boot@0x1FC02000:0x1FC024FF  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,-L"../../../../../../Program Files (x86)/Microchip/MPLAB C32 Suite/lib",-L"../../../../../../Program Files (x86)/Microchip/MPLAB C32 Suite/pic32mx/lib",-L".",-Map="${DISTDIR}/workin_v5_cleaning.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml 
+	
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),-L"../../../../../../Program Files (x86)/Microchip/MPLAB C32 Suite/lib",-L"../../../../../../Program Files (x86)/Microchip/MPLAB C32 Suite/pic32mx/lib",-L".",-Map="${DISTDIR}/workin_v5_cleaning.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml 
+	${MP_CC_DIR}\\xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/Puppy_Dog_v1.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/default
+	${RM} -r dist/default
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
